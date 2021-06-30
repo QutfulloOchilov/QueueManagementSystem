2a34ec69b61ef68325254e2c33812aca1daff040
Prolive <johndoe@example.com>
2021-06-30 13:01:46 +0500

    init web client project

---
+++ src/Web/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthorizationComponent } from './shared/pages/authorization/authorization.component';
+import { RegistrationComponent } from './shared/pages/registration/registration.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthorizationComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
