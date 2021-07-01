5910e4748cd829abbda33c67e25715b72a2be526
Prolive <johndoe@example.com>
2021-07-01 10:19:19 +0500

    Pages Created

---
+++ src/Web/src/app/pages/option-to-accept-reject-clients/option-to-accept-reject-clients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-option-to-accept-reject-clients',
+  templateUrl: './option-to-accept-reject-clients.component.html',
+  styleUrls: ['./option-to-accept-reject-clients.component.scss']
+})
+export class OptionToAcceptRejectClientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
