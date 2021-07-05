2244db12bcec6f8d363a76d3a6bf3bab8d79c8a0
Prolive <johndoe@example.com>
2021-07-05 13:51:09 +0500

    web - changes were made to the design

--- src/Web/src/app/app-routing.module.ts
+++ src/Web/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthorizationComponent } from './shared/pages/authorization/authorization.component';
-import { RegistrationComponent } from './shared/pages/registration/registration.component';
+import { AuthorizationComponent } from './pages/authorization/authorization.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { LayoutWorkerProfileComponent } from './shared/layouts/layout-worker-profile/layout-worker-profile.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/authorization', pathMatch: 'full'},
-  {path: 'authorization', component: AuthorizationComponent, },
-  {path: 'registration', component: RegistrationComponent}
+    {path: '', redirectTo: '/authorization', pathMatch: 'full'},
+    {path: 'authorization', component: AuthorizationComponent, },
+    {path: 'registration', component: RegistrationComponent},
+    {
+      path: '', component: LayoutWorkerProfileComponent, children: [
+        {path: 'dashboard', component: DashboardComponent}
+      ]
+    }
+  
 ];
 
 @NgModule({
