20e2d19415ff9d5b85e7ba60e292b21da449dc55
Shakhzod <shahzodniyozov03302000@gmail.com>
2021-09-01 13:57:53 +0500

    Added JsonConverter to the "Gender" and "Birthdate" properties in UserQueryModel.

--- src/Server/src/Application/QueueManagementSystem.Application/Users/QueryModels/Common/UserQueryModel.cs
+++ src/Server/src/Application/QueueManagementSystem.Application/Users/QueryModels/Common/UserQueryModel.cs
@@ -1,5 +1,7 @@
-﻿using QueueManagementSystem.Domain.Entities;
+﻿using QueueManagementSystem.Application.Attributes;
+using QueueManagementSystem.Domain.Entities;
 using System;
+using System.Text.Json.Serialization;
 
 namespace QueueManagementSystem.Application.Users.QueryModels.Common
 {
@@ -8,7 +10,11 @@ namespace QueueManagementSystem.Application.Users.QueryModels.Common
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string MiddleName { get; set; }
+
+		[JsonConverter(typeof(GenderConverter))]
 		public Gender Gender { get; set; }
+
+		[JsonConverter(typeof(ShortDateTimeConverter))]
 		public DateTime Birthdate { get; set; }
 		public string PhoneNumber { get; set; }
 		public string Email { get; set; }
